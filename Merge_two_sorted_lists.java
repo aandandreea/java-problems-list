62244c27686338f91e4cfbadab82304657244e89
Pisucho <126575169+aandandreea@users.noreply.github.com>
2025-11-06 14:57:36 +0200

    Create Merge_two_sorted_lists.java

---
+++ Merge_two_sorted_lists.java
@@ -0,0 +1,71 @@
+/* 
+21. Merge Two Sorted Lists
+
+Companies
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = null;
+        ListNode list3current = null;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                if (head == null) {
+                    head = new ListNode(list1.val);
+                    list3current = head;
+                } else {
+                    list3current.next = new ListNode(list1.val);
+                    list3current = list3current.next;
+                }
+
+                list1 = list1.next;
+            } else {
+                if (head == null) {
+                    head = new ListNode(list2.val);
+                    list3current = head;
+                } else {
+                    list3current.next = new ListNode(list2.val);
+                    list3current = list3current.next;
+                }
+                list2 = list2.next;
+            }
+
+        }
+        while (list1 != null) {
+            if (head == null) {
+                head = new ListNode(list1.val);
+                list3current = head;
+            } else {
+                list3current.next = new ListNode(list1.val);
+                list3current = list3current.next;
+            }
+            list1 = list1.next;
+        }
+        while (list2 != null) {
+            if (head == null) {
+                head = new ListNode(list2.val);
+                list3current = head;
+            } else {
+                list3current.next = new ListNode(list2.val);
+                list3current = list3current.next;
+            }
+            list2 = list2.next;
+        }
+        return head;
+    }
+
+}
