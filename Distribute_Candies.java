44fa29fae0bfb4e69e745d7986ba773b33ae473d
Andreea <126575169+aandandreea@users.noreply.github.com>
2026-03-18 17:57:34 +0200

    Create Distribute_Candies.java

---
+++ Distribute_Candies.java
@@ -0,0 +1,24 @@
+/*575. Distribute Candies
+
+Alice has n candies, where the ith candy is of type candyType[i]. Alice noticed that she started to gain weight, so she visited a doctor.
+
+The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). Alice likes her candies very much, and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
+
+Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
+
+*/
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        int n = candyType.length;
+        HashSet <Integer> set = new HashSet <>();
+        for(int i : candyType){
+            set.add(i);
+        }
+        if(set.size() == n/2){
+            return set.size();
+        } else if(set.size() >= n/2){
+            return n/2;
+        } else return set.size();
+    }
+}
