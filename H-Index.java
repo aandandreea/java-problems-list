e02f0cc2769c33126e57b3896ab25acb9b2e202a
Andreea <126575169+aandandreea@users.noreply.github.com>
2026-03-26 16:00:16 +0200

    Create H-Index.java

---
+++ H-Index.java
@@ -0,0 +1,33 @@
+/*274. H-Index
+
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+*/
+
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] count = new int[n + 1];
+
+        for (int c : citations) {
+            if (c >= n) {
+                count[n]++;
+            } else {
+                count[c]++;
+            }
+        }
+
+        int total = 0;
+
+        for (int i = n; i >= 0; i--) {
+            total += count[i];
+            if (total >= i) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
