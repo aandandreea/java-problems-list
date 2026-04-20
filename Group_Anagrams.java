0a9d1c93f13178692f0dd76fa78103669ee303cf
Andreea <126575169+aandandreea@users.noreply.github.com>
2026-04-20 15:04:26 +0300

    Create Group_Anagrams.java

---
+++ Group_Anagrams.java
@@ -0,0 +1,18 @@
+/*49. Group Anagrams
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.*/
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map <String,List<String>> map = new HashMap<>();
+
+        for(String s : strs){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            map.putIfAbsent(key,new ArrayList<>());
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
